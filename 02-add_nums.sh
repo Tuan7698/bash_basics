c14fb4a02943b4bd6f18fb11838ae4097d7e4644
Tuan Nguyen <tnguyen194@ggc.edu>
2025-10-24 21:43:21 -0400

    Complete 02-add_nums.sh main

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 echo "Enter your first number"
 read first
 echo "Enter your second number"
@@ -13,3 +11,11 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+read -p "Enter the width: " width
+read -p "Enter the height: " height
+
+# Calculate total pixels
+total=$((width * height))
+
+# Show the result
+echo "Total number of pixels: $total"
